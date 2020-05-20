900a6e4b998a9139df20a7d4dcb34707021e525e
presleyduggan <57386908+presleyduggan@users.noreply.github.com>
2020-05-20 12:08:47 -0700

    Add files via upload

---
+++ randomspy.py
@@ -0,0 +1,18 @@
+import numpy
+import pandas as pd
+import pandas_datareader as dr
+import random as random
+
+spy = []
+num = []
+
+num.append(random.randrange(0,506))
+
+data = open("spy.txt")
+
+for position, line in enumerate(data):
+
+    if position in num:
+        print(line)
+
+data.close()
